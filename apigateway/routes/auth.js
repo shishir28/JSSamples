187208ab96208f5e454283565bdd3100a97ed156
Shishir Kumar Mishra <shishir28@gmail.com>
2016-03-22 16:54:38 +0530

    token is generated during logon using passport-jwt and jwt-simple

--- apigateway/routes/auth.js
+++ apigateway/routes/auth.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('jwt-simple');
 var User = require('../models/MongoDB/user');
 
 module.exports = function (passport) {
     router.post('/account/register', function (req, res, done) {
         passport.authenticate('signup', function (err, user, info) {
-            var msg = err ? err : (info ? info : 'Registration Successful');
+            var msg = err ? err : (info ? info : 'Registration Successful!');
             res.send({
                 status: 200,
                 message: msg
@@ -14,14 +15,31 @@ module.exports = function (passport) {
     });
 
     // Authenticate a user.
-
     router.post('/account/login', function (req, res, done) {
         passport.authenticate('login', function (err, user, info) {
-            var msg = err ? err : (info ? info : 'Login Successful');
-            res.send({
-                status: 200,
-                message: msg
-            });
+            //res.json(401, { error: 'message' })
+            var msg = err ? err : (info ? info : 'Login Successful!');
+            if (err) {
+                res.send({
+                    status: 500,
+                    message: err
+                })
+            } else if (info) {
+                res.send({
+                    status: 401,
+                    message: info
+                });
+
+            } else {
+                var tokenSecret = 'infinitexxx';
+                var generatedToken = jwt.encode(user, tokenSecret);
+                res.send({
+                    status: 200,
+                    message: 'Login Successful!',
+                    token: generatedToken
+                });
+            }
+
         })(req, req.body.username, req.body.password, done);
     });
     return router;
